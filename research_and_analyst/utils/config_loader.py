eb3a45ea46703500a38332a568c9f673b40f092d
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-03-01 21:11:10 +0530

    files added

--- research_and_analyst/utils/config_loader.py
+++ research_and_analyst/utils/config_loader.py
@@ -2,7 +2,7 @@ import os
 import yaml
 import json
 from pathlib import Path
-from research_and_analyst.logger import GLOBAL_LOGGER as log
+from research_and_analyst.logger.custom_logger import GLOBAL_LOGGER as log
 from research_and_analyst.exception.custom_exception import ResearchAnalystException
 
 
