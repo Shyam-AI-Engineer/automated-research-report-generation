6aebcc427588e9e8d410ed78a7628d341609e063
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-03-08 20:26:57 +0530

    build graph

--- research_and_analyst/workflows/interview_workflow.py
+++ research_and_analyst/workflows/interview_workflow.py
@@ -13,3 +13,23 @@ from research_and_analyst.prompt_lib.prompt_locator import (
 )
 from research_and_analyst.logger import GLOBAL_LOGGER
 from research_and_analyst.exception.custom_exception import ResearchAnalystException
+
+class InterviewGraphBuilder:
+    """
+    A class responsible for constructing and managing the Interview Graph workflow.
+    Handles the process of:
+        1. Analyst generating questions.
+        2. Performing relevant web search.
+        3. Expert generating answers.
+        4. Saving the interview transcript.
+        5. Writing a summarized report section.
+    """
+
+    def __init__(self, llm, tavily_search):
+        """
+        Initialize the InterviewGraphBuilder with the LLM model and Tavily search tool.
+        """
+        self.llm = llm
+        self.tavily_search = tavily_search
+        self.memory = MemorySaver()
+        self.logger = GLOBAL_LOGGER.bind(module="InterviewGraphBuilder")
