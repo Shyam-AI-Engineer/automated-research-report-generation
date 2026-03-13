2117ab027f0c21479669807441321c72c9f3bf05
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-03-13 14:07:05 +0530

    import from fast api initiated

---
+++ research_and_analyst/api/templates/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from research_and_analyst.api.routes import report_routes
+from datetime import datetime
