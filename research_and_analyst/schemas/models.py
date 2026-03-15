0ee72332ddb082792da4211a3b4ecfa7225704ca
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-03-15 18:39:18 +0530

    code for models initiated

---
+++ research_and_analyst/schemas/models.py
@@ -0,0 +1,6 @@
+# models.py
+import operator
+from typing import Annotated, List
+from langgraph.graph import MessagesState
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
